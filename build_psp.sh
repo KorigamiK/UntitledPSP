4830796a2819d677cb7fae70658d1657684197dc
korigamik <korigamik@gmail.com>
2022-10-05 12:52:25 +0530

    add psp dist flag

--- build_psp.sh
+++ build_psp.sh
@@ -6,15 +6,17 @@ cd build/psp &&
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$PSPDEV/psp/share/pspdev.cmake -S ../../ -B . &&
 make &&
 
-if [[ $1 == "--package" ]]; then
-    mkdir -p dist &&
-    cp EBOOT.PBP dist/ &&
-    cp -r res dist/ &&
-    cp ../../README.md dist/ &&
-    cp ../../LICENSE dist/ &&
-    cp ../../CHANGELOG.md dist/ &&
-    cd dist &&
-    zip -r ../UNTITLED.$(date +%s).zip . &&
-    cd .. &&
-    rm -rf dist
+if [[ $1 == "--dist" ]]; then
+mkdir -p dist &&
+cp EBOOT.PBP dist/ &&
+cp -r res dist/ &&
+cp ../../README.md dist/ &&
+cp ../../LICENSE dist/ &&
+cp ../../CHANGELOG.md dist/ &&
+    if [[ $2 == "--package" ]]; then
+        cd dist &&
+        zip -r ../UNTITLED.$(date +%s).zip . &&
+        cd .. &&
+        rm -rf dist
+    fi
 fi
